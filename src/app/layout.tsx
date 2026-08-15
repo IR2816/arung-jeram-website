95d4e66e05e6909b817968fb4bded62b4db2052f
IR2816 <irfanthebb816@gmail.com>
2026-08-15 23:26:13 +0700

    Enable Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Outfit, Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
         <Preloader />
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
